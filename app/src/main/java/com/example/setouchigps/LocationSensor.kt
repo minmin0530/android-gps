8da89b1504e72845f485ce1cd433150d254eb238
minmin0530 <izumiyoshiki@icloud.com>
2024-08-07 17:50:08 +0900

    background gps

--- app/src/main/java/com/example/setouchigps/LocationSensor.kt
+++ app/src/main/java/com/example/setouchigps/LocationSensor.kt
@@ -3,15 +3,18 @@ package com.example.setouchigps
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Service
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.IBinder
 import android.os.Looper
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.location.*
 
-class LocationSensor(private val activity: Activity) {
+class LocationSensor(private val activity: Activity): Service() {
 
     private val fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(activity)
@@ -22,31 +25,81 @@ class LocationSensor(private val activity: Activity) {
     val location: LiveData<Location> = _location
 
     var run: Boolean = false
+    var count: Int = 0
+    val gpsPositions = mutableListOf<String>()
+    fun getCountA(): MutableList<String> { return gpsPositions }
 
     @SuppressLint("MissingPermission")
-    fun start() {
-        if (checkLocationPermission()) {
-            val locationRequest: LocationRequest.Builder =
-                LocationRequest.Builder(1000)
-                    .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
 
-            locationCallback = object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult) {
-                    for (location in locationResult.locations) {
-                        _location.postValue(location)
+        // ここにバックグラウンドで行いたい処理を書く
+        if (!run) {
+            run = true
+            Thread(Runnable {
+                //var count = 0
+                while (run) {
+                    count++
+                    if (checkLocationPermission()) {
+                        val locationRequest: LocationRequest.Builder =
+                            LocationRequest.Builder(5000)
+                                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+
+                        locationCallback = object : LocationCallback() {
+                            override fun onLocationResult(locationResult: LocationResult) {
+                                for (location in locationResult.locations) {
+
+                                    gpsPositions.add("lat = ${location.latitude}  long = ${location.longitude}")
+                                    _location.postValue(location)
+
+                                }
+                            }
+                        }
+
+                        fusedLocationClient.requestLocationUpdates(
+                            locationRequest.build(),
+                            locationCallback as LocationCallback,
+                            Looper.getMainLooper()
+                        )
+                    }
+
+
+
+
+                    try {
+                        Thread.sleep(5000) // 1秒間隔でカウントアップ
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
                     }
                 }
-            }
-
-            fusedLocationClient.requestLocationUpdates(
-                locationRequest.build(),
-                locationCallback as LocationCallback,
-                Looper.getMainLooper()
-            )
-
+            }).start()
         }
-        run = true
+        return START_NOT_STICKY
     }
+
+//    @SuppressLint("MissingPermission")
+//    fun start() {
+//        if (checkLocationPermission()) {
+//            val locationRequest: LocationRequest.Builder =
+//                LocationRequest.Builder(5000)
+//                    .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+//
+//            locationCallback = object : LocationCallback() {
+//                override fun onLocationResult(locationResult: LocationResult) {
+//                    for (location in locationResult.locations) {
+//                        _location.postValue(location)
+//                    }
+//                }
+//            }
+//
+//            fusedLocationClient.requestLocationUpdates(
+//                locationRequest.build(),
+//                locationCallback as LocationCallback,
+//                Looper.getMainLooper()
+//            )
+//
+//        }
+//        run = true
+//    }
 
     fun requestLocationPermission() {
         ActivityCompat.requestPermissions(
@@ -73,5 +126,12 @@ class LocationSensor(private val activity: Activity) {
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        run = false // ループを終了させる
+    }
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
     }
 }
--- app/src/main/java/com/example/setouchigps/MainActivity.kt
+++ app/src/main/java/com/example/setouchigps/MainActivity.kt
@@ -1,39 +1,84 @@
 package com.example.setouchigps
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import org.osmdroid.tileprovider.tilesource.XYTileSource
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var textView: TextView
     lateinit var startButton: Button
     lateinit var stopButton: Button
-
+    private lateinit var contryList: ListView
+//    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.textview)
-        startButton = findViewById(R.id.startButton)
-        stopButton = findViewById(R.id.stopButton)
+        textView = findViewById(R.id.textView)
+        startButton = findViewById(R.id.button4)
+        stopButton = findViewById(R.id.button5)
+//        mapView = findViewById(R.id.mapview)
 
+        // Listに表示する配列を用意する
+        val countries = mutableListOf<String>();
+
+        // Listに配置する
+    contryList = findViewById<ListView>(R.id.listview)
+    contryList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+
+//    val intent = Intent(this, LocationService::class.java)
+//    startForegroundService(intent)
+
+        val tileSource = XYTileSource("GSI Map", 5, 21, 256, ".png", arrayOf("https://cyberjapandata.gsi.go.jp/xyz/std/"))
+//        mapView.setTileSource(tileSource)
+//        mapView.setMultiTouchControls(true)
+//
+//        val mapController = mapView.controller
+        // 地図センター位置
         val locationSensor = LocationSensor(this)
         locationSensor.requestLocationPermission()
 
-        stopButton.isEnabled = false
-
+//        locationSensor.start()
+        locationSensor.onStartCommand(Intent(this, LocationSensor::class.java), 0, 0)
+//        stopButton.isEnabled = false
         locationSensor.location.observe(this, Observer {
-            textView.text = "lat = ${it.latitude}\n long = ${it.longitude}"
-        })
+            countries.add("${locationSensor.getCountA().last()}")
+            contryList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
 
+//            textView.text = "lat = ${it.latitude}\n long = ${it.longitude}"
+//            mapController.setZoom(21.0) // ズームレベル 15
+//            mapController.setCenter(GeoPoint(it.latitude,  it.longitude))
+        })
+//
         startButton.setOnClickListener {
             if (!locationSensor.run) {
-                locationSensor.start()
+                locationSensor.onStartCommand(Intent(this, LocationSensor::class.java), 0, 0)
                 startButton.isEnabled = false
                 stopButton.isEnabled = true
+
+//                locationSensor.location.observe(this, Observer {
+//                    textView.text = "lat = ${it.latitude}\n long = ${it.longitude}"
+//                    mapController.setZoom(21.0) // ズームレベル 15
+//                    mapController.setCenter(GeoPoint(it.latitude,  it.longitude))
+//                })
+
+//                val response = sendRequest(
+//                    url = "https://setouchi34.com/get_position",
+//                    method = "POST",
+//                    headers = mapOf(
+//                        "Content-Type" to "application/json"
+//                    ),
+//                    body = """{ "title": "Sample title", "body": "Sample body" }"""
+//                )
+//                textView.text = response.body
             }
         }
 
